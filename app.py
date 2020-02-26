2249054cc4bfd9fc54f7a20788b3f2d9a6599a9a
ProfFranky <francesco.sapioli@peano.it>
2020-02-26 19:31:01 +0100

    404, 500 pages and routes added

--- app.py
+++ app.py
@@ -121,6 +121,14 @@ from flask_login import login_required
 def profile():
     return render_template('profile.html')
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    return render_template('500.html'), 500
+
 ###############################################################
 
 
