0468f9239f5d280bb8460d23a861355c672c469e
ProfFranky <francesco.sapioli@peano.it>
2020-02-28 20:56:24 +0100

    trying to make the js work ..nothing for now

--- forms.py
+++ forms.py
@@ -8,18 +8,14 @@ class SignUpForm(FlaskForm):
     name = StringField('name', validators=[DataRequired()])
     surname = StringField('surname', validators=[DataRequired()])
     gender = SelectField(choices=[('unkwon', ''), ('male', 'MALE'), ('female', 'FEMALE'), ('other', 'OTHER')])
-    submit = SubmitField('Next')
-
-class CompleteSignUpForm(FlaskForm):#don't know if it is really neccesary using 3 forms
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
     email = StringField('Email', validators=[DataRequired(), Email()])
     password = PasswordField('Password', validators=[DataRequired(), Length(min=8, max=80)])
     password_con = PasswordField('Confirm', validators=[DataRequired(), EqualTo('password')])
-    submit = SubmitField('Next')
-
-class TutorSignUpForm(FlaskForm):#don't know if it is really neccesary using 3 forms
     degree = SelectField(choices=[('diploma', 'DIPLOMA'), ('bacheloer', 'BACHELOR'), ('master', 'MASTER')])
-    submit = SubmitField('Next')
+    submit = SubmitField('Sign Up')
+
+
 
 class LoginForm(FlaskForm):
     username = StringField('Username', validators=[DataRequired(), Length(min=4, max=15)])
---
+++ static/myJS.js
@@ -0,0 +1,11 @@
+function showHideStuff () {
+    if(document.getElementById('type') == 'student'){
+        document.getElementById('degreeGroup').style.display='none';
+        document.getElementById('degree').style.display='none';
+        document.getElementById('test').style.display='none';
+    }
+    else{
+        document.getElementById('degreeGroup').style.display='block';
+        document.getElementById('degree').style.display='block';
+    }
+}
